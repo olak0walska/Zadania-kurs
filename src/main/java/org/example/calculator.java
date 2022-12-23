51b2b9c487e895df0f6c88529b365d3fa2f48b97
Aleksandra Kowalska <okowalska57@gmail.com>
2022-12-23 16:10:20 +0100

    'project_setup'

---
+++ src/main/java/org/example/calculator.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class calculator {
+    public static int sum(int a, int b)
+    {
+        return a + b;
+    }
+    public static int subtract(int a, int b)
+    {
+        return a - b;
+    }
+    public static int multiply(int a, int b)
+    {
+        return a * b;
+    }
+    public static double division(int a, int b)
+    {
+        return a / b;
+    }
+}
